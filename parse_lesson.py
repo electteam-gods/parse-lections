9a488a4e194550e4c341e446610c405fc3bcac17
Ayyar Khalilov <super.ayar-04@yandex.ru>
2024-04-27 01:36:39 +0300

    exclude some titles from resulting json

--- parse_lesson.py
+++ parse_lesson.py
@@ -12,12 +12,30 @@ document = Document(filename)
 
 sections = []
 
+skip_next_paragraph = False
+
+excluded_titles = ["дополнительные материалы", "контрольные вопросы"]
+
 for para in document.paragraphs:
+
+    if (
+        para.style.name.startswith("Heading")
+        and para.text.lower().strip() in excluded_titles
+    ):
+        skip_next_paragraph = True
+        continue
+
+    if (
+        para.style.name.startswith("Heading")
+        and not para.text.lower().strip() in excluded_titles
+    ):
+        skip_next_paragraph = False
+
     if para.style.name.startswith("Heading") and len(para.text) > 0:
         sections.append({"title": para.text, "content": []})
         continue
     if len(sections) > 0 and len(para.text) > 0:
         sections[-1]["content"].append(para.text)
 
-f = open('results/' + filename + ".json", "w", encoding="utf-8")
+f = open("results/" + filename + ".json", "w", encoding="utf-8")
 f.write(json.dumps(sections))
